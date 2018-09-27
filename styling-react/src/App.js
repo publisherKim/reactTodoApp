a5b924e21eaf95857c4dc0e74122c6e0b7f5a10f
es-jukim <es-jukim@smilegate.com>
2018-09-27 15:00:43 +0900

    간편 문법

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import styles from  './App.css';
-console.log(styles);
 
 class App extends Component {
   render() {
