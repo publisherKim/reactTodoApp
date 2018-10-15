1ef8cfdeb6687503d613d774bb8f3af6bf9cd980
es-jukim <es-jukim@smilegate.com>
2018-10-15 12:53:08 +0900

    utils path alias, mixin use, variable use

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import styles from  './App.css';
+import classNames from 'classnames/bind';
+import styles from  './App.scss';
+import Button from './components/Button';
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
+    const isBlue = true;
     return (
-      <div className={classNames(styles.box, styles.blue)}>
-
+      <div className='wrap'>
+        <div className={cx('box', {
+          blue: isBlue
+        })}>
+          <div className={cx('box-inside')} />
+        </div>
+        <Button>버튼</Button>
       </div>
+
     );
   }
 }
