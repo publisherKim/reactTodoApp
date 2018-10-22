360cd490211479baa9297d88b827337f7d9a8a12
es-jukim <es-jukim@smilegate.com>
2018-10-22 16:02:33 +0900

    provider with store

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
