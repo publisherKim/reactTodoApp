3166931fca8e109c0af2a346d92381de238e8677
es-jukim <es-jukim@smilegate.com>
2018-10-22 09:34:11 +0900

    handleChange method add

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -4,11 +4,27 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
+  state = {
+    userTodo: ''
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      userTodo: value
+    })
+  }
+
   render() {
+    const { userTodo } = this.state;
+    const {
+      handleChange
+    } = this;
+    console.log(this, 'class안에서 this는 파일 그자체 여기선 App');
     return (
       <div>
         <PageTemplate>
-          <TodoInput></TodoInput>
+          <TodoInput onChange={handleChange} value={userTodo}></TodoInput>
           <TodoList></TodoList>
         </PageTemplate>
       </div>
