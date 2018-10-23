8da3a29733c09649e62495cb321826c6b55a10d3
es-jukim <es-jukim@smilegate.com>
2018-10-23 17:03:45 +0900

    list item modify

--- immutable-test/src/index.js
+++ immutable-test/src/index.js
@@ -1,7 +1,7 @@
 import { List, Map, fromJS } from 'immutable';
 
-const list1 = List([0, 1, 2, 3, 4]);
-console.log(list1);
+const list = List([0, 1, 2, 3, 4]);
+console.log(list);
 
 const list2 = List([
   Map({value: 1}),
@@ -15,3 +15,16 @@ const list3 = fromJS([
 ]);
 console.log(list3);
 console.log(list3.toJS());
+
+// list 값 읽어오기
+console.log(list.get(0));
+console.log(list2.getIn([0, 'value']));
+
+// list 아이템 수정
+const newList = list.set(0, Map({value: 10}));
+console.log(newList.toJS(), newList.getIn([0, 'value']));
+const newList2 = newList.setIn([0, 'value'], 1);
+console.log(newList2.toJS(), newList2.getIn([0, 'value']));
+const newList3 = newList.update(0, item => item.set('value', item.get('value') * 5));
+console.log(newList3.toJS(), newList3.getIn([0, 'value']));
+
