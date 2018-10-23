26eaa2055b07b1500d1b141863813a1dfd49a4cf
es-jukim <es-jukim@smilegate.com>
2018-10-23 15:18:26 +0900

    Immutable library 참조값 ì사용의 용이

--- immutable-test/src/index.js
+++ immutable-test/src/index.js
@@ -1,27 +1,28 @@
-let a = 7;
-let b = 7;
+import { Map } from 'immutable';
 
-let object1 = { a: 1, b: 2};
-let object2 = { a: 1, b: 2};
+let object1 = Map({
+  a: 1,
+  b: 2,
+  c: 3,
+  d: Map({
+    e: 4,
+    f: Map({
+      g: 5,
+      h: 6
+    })
+  })
+});
 
-const isBoolean = (arg1, arg2) => arg1 === arg2;
+let object2 = object1.setIn(['d', 'f', 'h'], 10);
+console.log(object1 === object2, object1, object2);
 
-
-// primative value vs reference value 
-const primativeValue = isBoolean(a, b);
-const referenceDiff = isBoolean(object1, object2);
-console.log(primativeValue, referenceDiff);
-
-let object3 = object1;
-const referenceSame = isBoolean(object1, object3);
-console.log(referenceSame);
-
-object3.c = 3;
-const isSameTo = isBoolean(object1, object3);
-console.log(isSameTo);
-
-let array1 = [0, 1, 2, 3, 4];
-let array2 = array1;
-array2.push(5);
-
-console.log(isBoolean(array1, array2));
+const data = Map({
+  a: 1,
+  b: 2,
+  c: Map({
+    d: 3,
+    e: 4,
+    f: 5
+  })
+});
+console.log(data);
