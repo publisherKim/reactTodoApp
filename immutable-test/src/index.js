3b3d3cd2d6fc142d6b0d5e3b58d70ca78407be3d
es-jukim <es-jukim@smilegate.com>
2018-10-23 16:22:56 +0900

    map get set merge using

--- immutable-test/src/index.js
+++ immutable-test/src/index.js
@@ -1,28 +1,37 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
-let object1 = Map({
+const data = fromJS({
   a: 1,
   b: 2,
-  c: 3,
-  d: Map({
-    e: 4,
-    f: Map({
-      g: 5,
-      h: 6
-    })
-  })
-});
-
-let object2 = object1.setIn(['d', 'f', 'h'], 10);
-console.log(object1 === object2, object1, object2);
-
-const data = Map({
-  a: 1,
-  b: 2,
-  c: Map({
+  c: {
     d: 3,
     e: 4,
     f: 5
-  })
+  }
 });
 console.log(data);
+
+// Immutable 객체를 일반 객체 형태로 변영하기
+const deserialized = data.toJS();
+console.log(deserialized);
+
+// 특정키의 값 불러오기
+const depth1 = data.get('a');
+const depth2 = data.getIn(['c', 'd']);
+console.log(depth1, depth2);
+
+// 특정키의 값 세팅하기
+const newData = data.set('a', 4);
+console.log(newData === data);
+const depthly = data.setIn(['c', 'd'], 4);
+console.log(depthly.getIn(['c', 'd']));
+
+// 여러 값 동시에 설정
+const newData2 = data.mergeIn(['c'], { d: 10, e: 5});
+console.log(newData2.getIn(['c', 'd']), newData2.getIn(['c', 'e']));
+const newData3 = data.setIn(['c', 'd'], 1).setIn(['c', 'e'], 4);
+console.log(newData3.getIn(['c', 'd']), newData3.getIn(['c', 'e']));
+
+// 최상위 값 설정
+const newData4 = data.merge({a: 10, b: 20});
+console.log(newData4.getIn('a'), newData4.getIn('b'));
