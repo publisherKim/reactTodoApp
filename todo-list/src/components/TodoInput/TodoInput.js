cba7dd44cbbfd926bcede2db474658e83fb1e543
es-jukim <es-jukim@smilegate.com>
2018-10-25 13:46:06 +0900

    css 경로 오류 수정

--- todo-list/src/components/TodoInput/TodoInput.js
+++ todo-list/src/components/TodoInput/TodoInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from 'TodoInput.scss';
+import styles from './TodoInput.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
