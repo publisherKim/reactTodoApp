599f1d16b4f036649d61940c4d612b17a18f8f3d
es-jukim <es-jukim@smilegate.com>
2018-10-25 15:18:00 +0900

    ããinput 값의 상태관리하기

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,11 +4,27 @@ import TodoInput from './components/TodoInput/TodoInput';
 import TodoList from './components/TodoList/TodoList';
 
 class App extends Component {
+  state = {
+    value: ''
+  }
+
+  handleChange(e) {
+    const {value} = e.target;
+
+    this.setState({
+      input: value
+    });
+  }
   render() {
+    const { input } = this.state;
+    const {
+      handleChange
+    } = this;
+    // handleChange  => this.handleChange 와 동치 문장이다.
     return (
       <div>
         <PageTemplate>
-          <TodoInput></TodoInput>
+          <TodoInput onChange={handleChange} value={input}></TodoInput>
           <TodoList></TodoList>
         </PageTemplate>
       </div>
