b29d82ab3bc880d878d3174dcb848e2dc1062c59
es-jukim <es-jukim@smilegate.com>
2018-10-25 10:42:24 +0900

    open color path 잘못 설치 삭제 webpack 설정 변경

--- todo-list/config/webpack.config.dev.js
+++ todo-list/config/webpack.config.dev.js
@@ -211,7 +211,6 @@ module.exports = {
               customize: require.resolve(
                 'babel-preset-react-app/webpack-overrides'
               ),
-              
               plugins: [
                 [
                   require.resolve('babel-plugin-named-asset-import'),
@@ -266,23 +265,25 @@ module.exports = {
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1
+                  importLoaders: 1,
                 },
               },
               {
                 loader: require.resolve('postcss-loader'),
                 options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
                     autoprefixer({
-                      borwsers: [
+                      browsers: [
                         '>1%',
                         'last 4 versions',
                         'Firefox ESR',
-                        'not ie < 9'
+                        'not ie < 9', // React doesn't support IE8 anyway
                       ],
-                      flexbox: 'no-2009'
+                      flexbox: 'no-2009',
                     }),
                   ],
                 },
@@ -294,24 +295,27 @@ module.exports = {
             use: [
               require.resolve('style-loader'),
               {
+                loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1
+                  importLoaders: 1,
                 },
               },
               {
                 loader: require.resolve('postcss-loader'),
                 options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
                     autoprefixer({
                       browsers: [
                         '>1%',
-                        'last4 versions',
+                        'last 4 versions',
                         'Firefox ESR',
-                        'not ie < 9',
+                        'not ie < 9', // React doesn't support IE8 anyway
                       ],
-                      flexbox: 'no-2009'
+                      flexbox: 'no-2009',
                     }),
                   ],
                 },
