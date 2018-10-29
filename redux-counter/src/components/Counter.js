70f5e35badd3de5b564bc4785b307aabf7cc3423
es-jukim <es-jukim@smilegate.com>
2018-10-29 17:18:21 +0900

    카운터 컴포넌트 수정

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
   return (
     <div
       className="Counter"
-      onClick={onIncrement}
+      onClick={() => onIncrement(index)}
       onContextMenu={(e)=>{
         e.preventDefault();
-        onDecrement();
+        onDecrement(index);
       }}
-      onDoubleClick={onSetColor}
+      onDoubleClick={() => onSetColor(index)}
       style={{
         backgroundColor: color
       }}
