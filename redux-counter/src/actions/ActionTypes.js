7133e7e8bbe8f0f7843e8418964a2b3204534a6b
es-jukim <es-jukim@smilegate.com>
2018-10-29 16:30:25 +0900

    CREATE REMOVE

--- redux-counter/src/actions/ActionTypes.js
+++ redux-counter/src/actions/ActionTypes.js
@@ -1,3 +1,6 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const SET_COLOR = 'SER_COLOR';
+
+export const CREATE = 'CREATE';
+export const REMOVe = 'REMOVE';
