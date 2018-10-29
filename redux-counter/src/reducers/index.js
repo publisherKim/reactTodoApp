c4b79f63e0246289390f26efd4f9e74b6f6e1085
es-jukim <es-jukim@smilegate.com>
2018-10-29 15:02:00 +0900

    reducers 생성 초기ë°값 필수

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -4,3 +4,25 @@ const initialState = {
   color: 'black',
   number: 0
 };
+
+export const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case types.INCREMENT:
+      return {
+        ...state,
+        number: state.number + 1
+      };
+    case types.DECREMENT:
+      return {
+        ...state,
+        number: state.number - 1
+      };
+    case types.SET_COLOR:
+      return {
+        ...state,
+        color: action.color
+      };
+    default:
+      return state;
+  }
+};
