1e6cd5031616fc88a9c29cb57fb98c4ffdc9797e
es-jukim <es-jukim@smilegate.com>
2018-10-30 15:39:41 +0900

    Immutable concept

---
+++ immutable-concept/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        immtable concept console을 통해 익히기
+      </div>
+    );
+  }
+}
+
+export default App;
