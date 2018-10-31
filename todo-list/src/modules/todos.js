676b9d528607cd06e622419556989f4b4dc086ea
es-jukim <es-jukim@smilegate.com>
2018-10-31 12:59:38 +0900

    todos모듈 생성 타입과 createAction을 액션 타입 생성 함수

---
+++ todo-list/src/modules/todos.js
@@ -0,0 +1,10 @@
+import { Map, List } from 'immutable';
+import { handleActions, createAction } from 'redux-actions';
+
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+export const insert = createAction(INSERT);
+export const toggle = createAction(TOGGLE);
+export const remove = createAction(REMOVE);
