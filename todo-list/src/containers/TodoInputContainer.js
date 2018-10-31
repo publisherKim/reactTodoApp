1616c62c240276380b137492c48c23335a46ca79
es-jukim <es-jukim@smilegate.com>
2018-10-31 14:22:26 +0900

    TodoInputContainer 생성과  connect설정

---
+++ todo-list/src/containers/TodoInputContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import TodoInput from '../components/TodoInput';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as inputActions from '../modules/input';
+import * as todosActions from '../modules/todos';
+
+class TodoInputContainer extends Component {
+  render() {
+    return (
+      <TodoInput></TodoInput>
+    );
+  }
+}
+
+export default connect(
+  (state) => ({
+    value: state.input.get('value')
+  }),
+  (dispatch) => ({
+    InputActions: bindActionCreators(inputActions, dispatch),
+    TodosActions: bindActionCreators(todosActions, dispatch)
+  })
+)(TodoInputContainer);
