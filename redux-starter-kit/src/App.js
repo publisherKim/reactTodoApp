31045fdc5adcea3143b885a1b67e67e79355c9f7
es-jukim <es-jukim@smilegate.com>
2018-11-02 15:09:03 +0900

    redux thunk를 í이용해서 post module 만들기

--- redux-starter-kit/src/App.js
+++ redux-starter-kit/src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 import * as counterActions from './modules/counter';
-import axios from 'axios';
+import * as postActions from './modules/post';
 
 
 class App extends Component {
+    loadData = () => {
+        const { PostActions, number} = this.props;
+        PostActions.getPost(number)
+    }
+
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts/1')
-            .then(response => console.log(response));
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.number !== prevProps.number) {
+            this.loadData();
+        }
     }
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, error, loading } = this.props;
 
         return (
             <div>
                 <h1>{number}</h1>
+                {
+                    loading
+                        ? (<h2>로딩중...</h2>)
+                        : (
+                            error
+                                ? (<h2>오류 발생!</h2>)
+                                : (
+                                    <div>
+                                        <h2>{post.title}</h2>
+                                        <p>{post.body}</p>
+                                    </div>
+                                )
+                    )
+                }
                 <button onClick={CounterActions.increment}>+</button>
                 <button onClick={CounterActions.decrement}>-</button>
                 <button onClick={CounterActions.incrementAsync}>async+</button>
@@ -27,9 +51,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
