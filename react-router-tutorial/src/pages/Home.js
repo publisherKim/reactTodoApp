093ffe29ed1d5fd98c7bac002f733ca4f046e317
es-jukim <es-jukim@smilegate.com>
2018-11-05 14:08:57 +0900

    자바스크립트 함수를 통한 라우트 이동예시

--- react-router-tutorial/src/pages/Home.js
+++ react-router-tutorial/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Home = () => {
+const Home = ({history}) => {
   return (
     <div>
       <h2>홈</h2>
+      <button onClick={() => {
+        // 조건등에 대한 처리가 가능하다.
+        history.push('/about')
+      }}>자바스크립트를 사용하여 이동</button>
     </div>
   )
 };
 
-export default Home
+export default Home;
