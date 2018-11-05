c262822c4807fafd4fc40eee4701888a1b4b83d8
es-jukim <es-jukim@smilegate.com>
2018-11-05 11:25:29 +0900

    query string 값 활용하기

--- react-router-tutorial/src/pages/About.js
+++ react-router-tutorial/src/pages/About.js
@@ -3,10 +3,10 @@ import queryString from 'query-string';
 
 const About = ({location, match}) => {
   const query = queryString.parse(location.search);
-  console.log(query)
+  const { color } = query;
   return (
     <div>
-      <h2>소개</h2>
+      <h2 style={{color}}>소개</h2>
       <p>
         안녕하세요, 저는 {match.params.name} 입니다.
       </p>
