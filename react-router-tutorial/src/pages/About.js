dacddc00277de466dc65c21ed7ace130523b1950
es-jukim <es-jukim@smilegate.com>
2018-11-05 11:00:23 +0900

    기본 라우트 준비

---
+++ react-router-tutorial/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'ract';
+
+const About = () => {
+  return (
+    <div>
+      <h2>소개</h2>
+      <p>
+        안녕하세요, 저는 리액트 라우터 입니다.
+      </p>
+    </div>
+  )
+};
+
+export default About
