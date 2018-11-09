0706084974e67a7fd6892239fcfe803309916e16
es-jukim <es-jukim@smilegate.com>
2018-11-09 12:21:07 +0900

    .env 호환경변수 파일 생성

--- blog/blog-backend/index.js
+++ blog/blog-backend/index.js
@@ -1,31 +1,37 @@
-  // koa-router 모듈의 기본적인 사용법
-  const koa = require('koa');
-  const Router = require('koa-router');
+require('dotenv').config();
 
-  const app = new koa();
-  const router = new Router();
+// koa-router 모듈의 기본적인 사용법
+const koa = require('koa');
+const Router = require('koa-router');
 
-  // 라우터 설정
-  router.get('/', (ctx) => {
-    ctx.body = '홈';
-  });
+const app = new koa();
+const router = new Router();
 
-  router.get('/about/:name?', (ctx) => {
-    const { name } = ctx.params;
-    ctx.body = name ? `${name}의 소개` : '소개';
-  });
+const {
+  PORT: port,
+  MONGO_URI: mongoURI
+} = process.env
 
-  router.get('/posts', (ctx) => {
-    const { id } = ctx.query;
-    console.log(id);
-    // id의 존재 유무에 따라 다른 결과 출력
-    ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-  });
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  // id의 존재 유무에 따라 다른 결과 출력
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+});
 
 
-  // app 인스턴스에 라우터 적용
-  app.use(router.routes()).use(router.allowedMethods());
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
-  app.listen(4000, () => {
-    console.log('listening to port 4000');
-  });
+app.listen(port, () => {
+  console.log(`listening to port: ${port}`);
+});
